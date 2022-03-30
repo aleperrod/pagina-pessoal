44dee644610c27778e82d1abd75ab7e25306a9fa
Alessandro Pereira Rodrigues <ale.per.rod@gmail.com>
2022-03-29 21:42:34 -0300

    Cartão de visita concluído (site ainda em construção).

--- src/components/Oferecimento.tsx
+++ src/components/Oferecimento.tsx
@@ -5,8 +5,8 @@ interface OferecimentoProps {
 export default function Oferecimento(props: OferecimentoProps){
     return (
         <div className={`flex flex-col justify-center items-center
-        p-2 rounded-md text-3xl font-extralight  bg-[#1E313B30] text-red-500
-        hover:text-red-600 `}
+        p-2 rounded-md text-3xl font-extralight  bg-[#1E313B30] text-red-700
+        hover:text-red-500 `}
         >
             <div>
                 {props.titulo}
